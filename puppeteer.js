9e241fa50b2824f0987a6921fe3b9cd7fa4f29b6
ErikRice <erik.m.rice12@gmail.com>
2021-11-16 23:20:26 -0500

    initial push

---
+++ puppeteer.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    try{
+        const browser = await puppeteer.launch({
+            executablePath: '/usr/bin/chromium-browser'
+          })
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3000/addUsers.html');
+        await page.waitForSelector('.btn btn-primary');
+        await page.click('.btn btn-primary')
+        return await page.screenshot({ path:'example.png' } )
+
+        await broswer.close();
+    }
+    catch (err) {
+        console.log(err)
+    }
+})()
+
+
+
